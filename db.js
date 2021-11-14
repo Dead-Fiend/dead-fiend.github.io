69b85f197ea05d153bbcd7b152d850b594e578cc
Dead-Fiend <83966220+Dead-Fiend@users.noreply.github.com>
2021-11-14 22:37:19 +0300

    upd

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
+/*module.exports = new Sequelize(
     `${process.env.DB_NAME}`,
     `${process.env.DB_USER}`,
     `${process.env.DB_PASSWORD}`,
@@ -8,6 +8,17 @@ module.exports = new Sequelize(
         dialect: 'postgres',
         host: `${process.env.DB_HOST}`,
         port: `${process.env.DB_PORT}`,
+
+    }
+)*/
+module.exports = new Sequelize(
+    `datosabmmm3i9o`,
+    `iryydpveublrtf`,
+    `baa756f4bc3c6d0ddbf782b0f34a960dce71e3a36641e68f0c2623449c3d8248`,
+    {
+        dialect: 'postgres',
+        host: `ec2-34-250-19-18.eu-west-1.compute.amazonaws.com`,
+        port: `5432`,
 
     }
 )
