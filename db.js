bffa808233ad3f427375276222bec9a55e0d267f
Dead-Fiend <83966220+Dead-Fiend@users.noreply.github.com>
2021-11-14 22:59:27 +0300

    upd

--- db.js
+++ db.js
@@ -11,14 +11,18 @@ const {Sequelize} = require('sequelize')
 
     }
 )*/
-module.exports = new Sequelize(
-    `datosabmmm3i9o`,
-    `iryydpveublrtf`,
-    `baa756f4bc3c6d0ddbf782b0f34a960dce71e3a36641e68f0c2623449c3d8248`,
-    {
-        dialect: 'postgres',
-        host: `ec2-34-250-19-18.eu-west-1.compute.amazonaws.com`,
-        port: `5432`,
 
-    }
-)
+module.exports = new Sequelize({
+    database: `${process.env.DB_NAME}`,
+    username: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASSWORD}`,
+    host: `${process.env.DB_HOST}`,
+    port: `${process.env.DB_PORT}`,
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true, // This will help you. But you will see nwe error
+            rejectUnauthorized: false // This line will fix new error
+        }
+    },
+});
