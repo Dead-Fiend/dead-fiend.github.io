63b58e65e64cd424f9e3a270e187c1f4ef444beb
Dead-Fiend <83966220+Dead-Fiend@users.noreply.github.com>
2021-11-14 21:40:32 +0300

    upd

--- db.js
+++ db.js
@@ -17,6 +17,6 @@ const {Sequelize} = require('sequelize')
 module.exports = new Sequelize('postgres://iryydpveublrtf:baa756f4bc3c6d0ddbf782b0f34a960dce71e3a36641e68f0c2623449c3d8248@ec2-34-250-19-18.eu-west-1.compute.amazonaws.com:5432/datosabmmm3i9o', {
     dialect: 'postgres',
     dialectOptions: {
-        ssl: true
+        ssl: false
     }
 });
