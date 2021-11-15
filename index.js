ae7d08ba5b0172a94e1986e9e76f717e69c4dc2e
Dead-Fiend <83966220+Dead-Fiend@users.noreply.github.com>
2021-11-15 13:29:00 +0300

    upd

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('*', (req, res) => {
 */
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/src/index.js'))
+    res.sendFile(path.join(__dirname + '/build/index.html'))
 })
 
 const start = async() => {
