5845938e5d4f8e5e604dba4cef4c0e0e5a834e85
Dead-Fiend <83966220+Dead-Fiend@users.noreply.github.com>
2021-11-15 13:20:43 +0300

    upd

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ app.use(fileUpload({}))
 app.use('/api', router)
 
 app.use(errorHandler)
-
+/*
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'))
 })
-
+*/
 const start = async() => {
     try {
         await sequelize.authenticate()
