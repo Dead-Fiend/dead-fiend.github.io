0dad3219cef0a2c08b1ef854b24ffe0ffc09e4ab
Dead-Fiend <83966220+Dead-Fiend@users.noreply.github.com>
2021-11-15 20:19:57 +0300

    upd

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(errorHandler)
 
 app.get('*', (req, res) => {
     //res.sendFile(path.join(__dirname, 'build', 'index.html'))
-    res.sendFile(path.join('build', 'index.html'))
+    res.sendFile(path.join('/build/index.html'))
 })
 const start = async() => {
     try {
