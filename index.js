2d84a8d2f5da629dc6b04f24103307c4c93fb9b9
Dead-Fiend <83966220+Dead-Fiend@users.noreply.github.com>
2021-11-17 22:41:09 +0300

    upd

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use('https://frstshop.herokuapp.com/', express.static(path.join(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, 'static')))
 
 app.use(fileUpload({}))
 app.use('/api', router)
